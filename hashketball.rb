249a3fb3024cc021a5d77005b14338a352d075de
dazzling-archive-8607 <mnavoy4@gmail.com>
2020-05-23 15:02:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@
 def game_hash
   {
     :home => {
-      :team_name => "",
+      :team_name => "Brooklyn Nets",
       :colors => [],
       :players => [
         {
@@ -62,7 +62,67 @@ def game_hash
         }
         ]
     },
-    
+    :away => {
+      :team_name => "",
+      :colors => [],
+      :players => [
+        {
+        :player_name => "",
+        :number => 1,
+        :shoe => "",
+        :points => 2,
+        :rebounds => 8,
+        :assists => 4,
+        :steals => 5,
+        :blocks => 5,
+        :slam_sunks => 4
+        }, 
+        {
+        :player_name => "",
+        :number => 1,
+        :shoe => "",
+        :points => 2,
+        :rebounds => 8,
+        :assists => 4,
+        :steals => 5,
+        :blocks => 5,
+        :slam_sunks => 4
+        },
+        {
+        :player_name => "",
+        :number => 1,
+        :shoe => "",
+        :points => 2,
+        :rebounds => 8,
+        :assists => 4,
+        :steals => 5,
+        :blocks => 5,
+        :slam_sunks => 4
+        },
+        {
+        :player_name => "",
+        :number => 1,
+        :shoe => "",
+        :points => 2,
+        :rebounds => 8,
+        :assists => 4,
+        :steals => 5,
+        :blocks => 5,
+        :slam_sunks => 4
+        },
+        {
+        :player_name => "",
+        :number => 1,
+        :shoe => "",
+        :points => 2,
+        :rebounds => 8,
+        :assists => 4,
+        :steals => 5,
+        :blocks => 5,
+        :slam_sunks => 4
+        }
+        ]
+    }
   }
   
 end
