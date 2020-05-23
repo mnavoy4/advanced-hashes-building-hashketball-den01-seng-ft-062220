232376b58c2bf90eff6d70df9c02c84901320ddb
dazzling-archive-8607 <mnavoy4@gmail.com>
2020-05-23 15:07:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,71 +3,71 @@ def game_hash
   {
     :home => {
       :team_name => "Brooklyn Nets",
-      :colors => [],
+      :colors => ["Black", "White"],
       :players => [
         {
-        :player_name => "",
-        :number => 1,
-        :shoe => "",
-        :points => 2,
-        :rebounds => 8,
-        :assists => 4,
-        :steals => 5,
-        :blocks => 5,
-        :slam_sunks => 4
+        :player_name => "Alan Anderson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_sunks => 1
         }, 
         {
-        :player_name => "",
-        :number => 1,
-        :shoe => "",
-        :points => 2,
-        :rebounds => 8,
-        :assists => 4,
-        :steals => 5,
-        :blocks => 5,
-        :slam_sunks => 4
+        :player_name => "Reggie Evans",
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_sunks => 7
         },
         {
-        :player_name => "",
-        :number => 1,
-        :shoe => "",
-        :points => 2,
-        :rebounds => 8,
-        :assists => 4,
-        :steals => 5,
-        :blocks => 5,
-        :slam_sunks => 4
+        :player_name => "Brook Lopez",
+        :number => 11,
+        :shoe => 17,
+        :points => 17,
+        :rebounds => 19,
+        :assists => 10,
+        :steals => 3,
+        :blocks => 1,
+        :slam_sunks => 15
         },
         {
-        :player_name => "",
+        :player_name => "Mason Plumlee",
         :number => 1,
-        :shoe => "",
-        :points => 2,
-        :rebounds => 8,
-        :assists => 4,
-        :steals => 5,
-        :blocks => 5,
-        :slam_sunks => 4
+        :shoe => 19,
+        :points => 26,
+        :rebounds => 11,
+        :assists => 6,
+        :steals => 3,
+        :blocks => 8,
+        :slam_sunks => 5
         },
         {
-        :player_name => "",
-        :number => 1,
-        :shoe => "",
-        :points => 2,
-        :rebounds => 8,
-        :assists => 4,
-        :steals => 5,
-        :blocks => 5,
-        :slam_sunks => 4
+        :player_name => "Jason Terry",
+        :number => 31,
+        :shoe => 15,
+        :points => 19,
+        :rebounds => 2,
+        :assists => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_sunks => 1
         }
         ]
     },
     :away => {
-      :team_name => "",
-      :colors => [],
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
       :players => [
         {
-        :player_name => "",
+        :player_name => "Jeff Adrien",
         :number => 1,
         :shoe => "",
         :points => 2,
