76363fc7ebf7d7bf0fa5cbc07bafc9521f85f03e
dazzling-archive-8607 <mnavoy4@gmail.com>
2020-05-23 15:01:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,9 +15,54 @@ def game_hash
         :steals => 5,
         :blocks => 5,
         :slam_sunks => 4
+        }, 
+        {
+        :player_name => "",
+        :number => 1,
+        :shoe => "",
+        :points => 2,
+        :rebounds => 8,
+        :assists => 4,
+        :steals => 5,
+        :blocks => 5,
+        :slam_sunks => 4
+        },
+        {
+        :player_name => "",
+        :number => 1,
+        :shoe => "",
+        :points => 2,
+        :rebounds => 8,
+        :assists => 4,
+        :steals => 5,
+        :blocks => 5,
+        :slam_sunks => 4
+        },
+        {
+        :player_name => "",
+        :number => 1,
+        :shoe => "",
+        :points => 2,
+        :rebounds => 8,
+        :assists => 4,
+        :steals => 5,
+        :blocks => 5,
+        :slam_sunks => 4
+        },
+        {
+        :player_name => "",
+        :number => 1,
+        :shoe => "",
+        :points => 2,
+        :rebounds => 8,
+        :assists => 4,
+        :steals => 5,
+        :blocks => 5,
+        :slam_sunks => 4
         }
         ]
-    }
+    },
+    
   }
   
 end
